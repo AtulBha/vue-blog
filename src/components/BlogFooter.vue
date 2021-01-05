58f0d3db96e99d0ab28c2f58df0959d2dde884d5
Atul Bhardwaj <atulbha@gmail.com>
2021-01-04 19:19:23 -0500

    Update BlogFooter.vue

--- src/components/BlogFooter.vue
+++ src/components/BlogFooter.vue
@@ -2,7 +2,7 @@
   <transition appear name="v--mask">
     <footer v-once class="blog__footer">
       <div class="signature cosmicjs">
-        Checkout<a href="https://github.com/AtulBha" target="_blank">my GitHub</a>
+        Checkout <a href="https://github.com/AtulBha" target="_blank">my GitHub</a>
       </div>
     </footer>
   </transition>
